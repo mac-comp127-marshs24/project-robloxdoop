950bd714c3007b0f5ab40783072691090b1cbbbd
DesOPS <dosemwen@macalester.edu>
2024-04-15 20:54:40 -0500

    HomeScreen Created! 4/15/2024

--- src/TheMotherBoard.java
+++ src/TheMotherBoard.java
@@ -1,26 +1,9 @@
-import edu.macalester.graphics.CanvasWindow;
-import edu.macalester.graphics.Image;
+
 
 public class TheMotherBoard {
 
-    private static Image runwayBackground;
-
-    public TheMotherBoard(){
-
-    CanvasWindow canvas = new CanvasWindow("Project Runway",800, 600);
-
-    runwayBackground = new Image(0,0);
-    runwayBackground.setScale(1.5,1.5);
-    runwayBackground.setImagePath("RunwayBackground.jpeg");
-
-    canvas.add(runwayBackground);
-    }
-
-
-
     public static void main(String[] args) {
-        new TheMotherBoard();
-        
+        new HomeScreen();  
     }
 
     
