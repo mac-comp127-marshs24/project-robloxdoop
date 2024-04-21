fc85998a726fdb6b97ed1631840b27fba6c80752
samiramhussein <150379833+samiramhussein@users.noreply.github.com>
2024-04-21 18:24:09 -0500

    m

--- src/TheMotherBoard.java
+++ src/TheMotherBoard.java
@@ -12,38 +12,43 @@ import ThemesOutfit.Beach;
 
 
 public class TheMotherBoard {
+    Random randNum = new Random();
+    List<Theme> themes = new ArrayList<>()
+    randNum.nextInt(themes.size())
 
-    // public static void main(String[] args) {
-    //     new TheMotherBoard().generateRandomThemes();  
-    // }
+    public static void main(String[] args) {
+        new TheMotherBoard().generateRandomThemes();  
+    }
+//Method Works only with Theme Interface or superclass
+    //having problems with creating this, Ask Rama Destiny and Hadly
+    themes.add(new School());
+    themes.add(new Winter());
+    themes.add(new Beach());
 
-    // Winter winterGloves = new Winter(); 
 
-    // public void generateRandomThemes() {
-    //     List<Theme> themes = new ArrayList<>(); //Method Works only with Theme Interface or superclass
-    //     //having problems with creating this, Ask Rama Destiny and Hadly
-    //     themes.add(new School());
-    //     themes.add(new Winter());
-    //     themes.add(new Beach());
+    public void generateRandomThemes() {
+      
      
 
-    //     Random themeGenerator = new Random();
-    //     List<Theme> selectedThemes = new ArrayList<>();
-    //     for (int i = 0; i < 3; i++){
-    //         int index = themeGenerator.nextInt(themes.size());
-    //         selectedThemes.add(themes.remove(index)); 
+    
+        List<Theme> selectedThemes = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            int index = themeGenerator.nextInt(themes.size());
+            selectedThemes.add(themes.remove(index)); 
         
 
-    // }
+    }
+}
+}
 
     
 
 
-    public static void main(String[] args) {
-        new ScreenManager();  
+//     public static void main(String[] args) {
+//         new ScreenManager();  
         
-    }
-}
+//     }
+// }
 
 
 
