fc00fa5a288e3d104b566908691b2826e79780f3
DesOPS <dosemwen@macalester.edu>
2024-05-02 19:13:41 -0500

    Update ClosetManager.java

--- src/ClosetManager.java
+++ src/ClosetManager.java
@@ -361,6 +361,7 @@ public class ClosetManager {
             if(canvas.getElementAt(event.getPosition()) == runwayButton){
                 canvas.removeAll();
                 ScreenManager.theRunway();
+                
             }
 
         }
